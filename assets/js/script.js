0b9cfb663e42f44ea2c045454c80946805d57283
Iago Dantas <iagodantas.8@gmail.com>
2022-06-27 09:23:35 -0300

    stop counter function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,7 @@ const CURRENT_NUMBER = document.getElementById('currentNumber');
 const INCREMENT = document.getElementById('increment');
 const DECREMENT = document.getElementById('decrement');
 
-
-
-function turnRed(){
+const turnRed = ()=>{
 	if(count < 0){
 		CURRENT_NUMBER.style.color = '#D4133C';
 	}
@@ -14,15 +12,25 @@ function turnRed(){
 		CURRENT_NUMBER.style.color = '#000';
 	}
 }
-
 INCREMENT.addEventListener("click", function () {
-	count++;
-	CURRENT_NUMBER.innerHTML = count;
-	turnRed();
+	if(count <= 49 ){
+		count++;
+		CURRENT_NUMBER.innerHTML = count;
+		turnRed();
+	}
+	else{
+		INCREMENT.attributes.add("disabled");
+	}
 })
 
 DECREMENT.addEventListener("click", function () {
-	count--;
-	CURRENT_NUMBER.innerHTML = count;
-	turnRed();
+	if(count >= -49 ){
+		count--;
+		CURRENT_NUMBER.innerHTML = count;
+		turnRed();
+	}
+	else{
+		DECREMENT.attributes.add("disabled");
+	}
 })
+
