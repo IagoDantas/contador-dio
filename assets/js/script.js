7170cc4e4073276e9b71920df17f8ab52383b579
Iago Dantas <iagodantas.8@gmail.com>
2022-06-27 06:28:12 -0300

    make text red if less than 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,23 @@ let count = 0;
 
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
+function turnRed(){
+	if(count < 0){
+		CURRENT_NUMBER.style.color = '#D4133C';
+	}
+	else{
+		CURRENT_NUMBER.style.color = '#000';
+	}
+}
+
 function increment() {
 	count++;
 	CURRENT_NUMBER.innerHTML = count;
+	turnRed();
 }
 
 function decrement() {
 	count--;
 	CURRENT_NUMBER.innerHTML = count;
+	turnRed();
 }
